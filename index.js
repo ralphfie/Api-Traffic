2e65773c64afc38c3eb37038d333a97ff1820513
ralphfie <70000130+ralphfie@users.noreply.github.com>
2020-08-24 09:26:57 +0800

    intial

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const apiTraffic = require('expressjs-apitraffic')
+
+app.use(
+  apiTraffic.forRoot({
+    elasticsearch: 'http://elasticsearch-syukur.apps.cp.tmrnd.com.my/',
+    elasticsearchIndexPrefix: 'api-test-syukur'
+  })
+);
+
+app.get('/', (req, res) => res.send('Hello World!!'));
+app.get('/haha', (req, res) => {
+    res.status(400).send({message: 'error ni boh'});
+});
+
+app.listen(port, () =>
+  console.log(`Example app listening at http://localhost:${port}`)
+);
